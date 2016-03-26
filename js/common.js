e630106b8a4043acbb11020ca9d534b221fa9da7
AnastasiaSerb <anastasiya.serbinovskaya@mail.ru>
2016-03-26 18:51:31 +0200

    index
    go

---
+++ js/common.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel({
+  	items : 1,
+    itemsCustom : false,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [980,1],
+    itemsTablet: [768,1],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false,
+ 
+    //Basic Speeds
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+ 
+    //Autoplay
+    autoPlay : true
+  });
+ 
+});
